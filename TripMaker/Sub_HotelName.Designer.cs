5ff9914741c1fc14fbb4a2b2e1dcb97e029a54fa
Dip <dipachorjee@gmail.com>
2025-08-30 22:15:20 +0600

    First commit

---
+++ TripMaker/Sub_HotelName.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TripMaker
+{
+    partial class Sub_HotelName
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbResturant = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResturant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(186, 59);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(117, 23);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Hotel name";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(549, 59);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(71, 26);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.ForeColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(75, 156);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(551, 4);
+            this.panel1.TabIndex = 12;
+            // 
+            // pbResturant
+            // 
+            this.pbResturant.Image = global::TripMaker.Properties.Resources.Hotel_Card;
+            this.pbResturant.Location = new System.Drawing.Point(16, 15);
+            this.pbResturant.Margin = new System.Windows.Forms.Padding(2);
+            this.pbResturant.Name = "pbResturant";
+            this.pbResturant.Size = new System.Drawing.Size(158, 128);
+            this.pbResturant.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResturant.TabIndex = 92;
+            this.pbResturant.TabStop = false;
+            // 
+            // Sub_HotelName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pbResturant);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Sub_HotelName";
+            this.Size = new System.Drawing.Size(698, 165);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResturant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbResturant;
+    }
+}
---
+++ TripMaker/TrainBooking.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace TripMaker
+{
+    partial class TrainBooking
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.aaa = new System.Windows.Forms.Label();
+            this.aa = new System.Windows.Forms.Label();
+            this.btnbook = new System.Windows.Forms.Button();
+            this.lblprice = new System.Windows.Forms.Label();
+            this.lblarvT = new System.Windows.Forms.Label();
+            this.lblEnding = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblstart = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbldepT = new System.Windows.Forms.Label();
+            this.Lblname = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbResturant = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResturant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(605, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 19);
+            this.label1.TabIndex = 76;
+            this.label1.Text = "Fare";
+            // 
+            // aaa
+            // 
+            this.aaa.AutoSize = true;
+            this.aaa.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.aaa.ForeColor = System.Drawing.Color.Black;
+            this.aaa.Location = new System.Drawing.Point(488, 52);
+            this.aaa.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.aaa.Name = "aaa";
+            this.aaa.Size = new System.Drawing.Size(84, 19);
+            this.aaa.TabIndex = 75;
+            this.aaa.Text = "Arrival Time";
+            // 
+            // aa
+            // 
+            this.aa.AutoSize = true;
+            this.aa.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.aa.ForeColor = System.Drawing.Color.Black;
+            this.aa.Location = new System.Drawing.Point(353, 52);
+            this.aa.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.aa.Name = "aa";
+            this.aa.Size = new System.Drawing.Size(113, 19);
+            this.aa.TabIndex = 74;
+            this.aa.Text = "Departure Time";
+            // 
+            // btnbook
+            // 
+            this.btnbook.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnbook.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnbook.Location = new System.Drawing.Point(585, 118);
+            this.btnbook.Margin = new System.Windows.Forms.Padding(2);
+            this.btnbook.Name = "btnbook";
+            this.btnbook.Size = new System.Drawing.Size(68, 24);
+            this.btnbook.TabIndex = 73;
+            this.btnbook.Text = "Book Now";
+            this.btnbook.UseVisualStyleBackColor = false;
+            this.btnbook.Click += new System.EventHandler(this.btnbook_Click);
+            // 
+            // lblprice
+            // 
+            this.lblprice.AutoSize = true;
+            this.lblprice.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.lblprice.ForeColor = System.Drawing.Color.Black;
+            this.lblprice.Location = new System.Drawing.Point(600, 82);
+            this.lblprice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblprice.Name = "lblprice";
+            this.lblprice.Size = new System.Drawing.Size(33, 19);
+            this.lblprice.TabIndex = 72;
+            this.lblprice.Text = "350";
+            // 
+            // lblarvT
+            // 
+            this.lblarvT.AutoSize = true;
+            this.lblarvT.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.lblarvT.ForeColor = System.Drawing.Color.Black;
+            this.lblarvT.Location = new System.Drawing.Point(497, 82);
+            this.lblarvT.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblarvT.Name = "lblarvT";
+            this.lblarvT.Size = new System.Drawing.Size(70, 19);
+            this.lblarvT.TabIndex = 71;
+            this.lblarvT.Text = "03:00 Pm";
+            // 
+            // lblEnding
+            // 
+            this.lblEnding.AutoSize = true;
+            this.lblEnding.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnding.ForeColor = System.Drawing.Color.Black;
+            this.lblEnding.Location = new System.Drawing.Point(270, 83);
+            this.lblEnding.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEnding.Name = "lblEnding";
+            this.lblEnding.Size = new System.Drawing.Size(55, 19);
+            this.lblEnding.TabIndex = 70;
+            this.lblEnding.Text = "Dhaka";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(178, 83);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.TabIndex = 69;
+            this.label6.Text = "Ending Point :";
+            // 
+            // lblstart
+            // 
+            this.lblstart.AutoSize = true;
+            this.lblstart.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstart.ForeColor = System.Drawing.Color.Black;
+            this.lblstart.Location = new System.Drawing.Point(273, 54);
+            this.lblstart.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblstart.Name = "lblstart";
+            this.lblstart.Size = new System.Drawing.Size(67, 19);
+            this.lblstart.TabIndex = 68;
+            this.lblstart.Text = "Rajshahi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(178, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 17);
+            this.label3.TabIndex = 67;
+            this.label3.Text = "Starting Point :";
+            // 
+            // lbldepT
+            // 
+            this.lbldepT.AutoSize = true;
+            this.lbldepT.Font = new System.Drawing.Font("Century Gothic", 10.5F);
+            this.lbldepT.ForeColor = System.Drawing.Color.Black;
+            this.lbldepT.Location = new System.Drawing.Point(370, 82);
+            this.lbldepT.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbldepT.Name = "lbldepT";
+            this.lbldepT.Size = new System.Drawing.Size(70, 19);
+            this.lbldepT.TabIndex = 66;
+            this.lbldepT.Text = "12:30 Pm";
+            // 
+            // Lblname
+            // 
+            this.Lblname.AutoSize = true;
+            this.Lblname.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblname.ForeColor = System.Drawing.Color.Red;
+            this.Lblname.Location = new System.Drawing.Point(332, 7);
+            this.Lblname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Lblname.Name = "Lblname";
+            this.Lblname.Size = new System.Drawing.Size(83, 23);
+            this.Lblname.TabIndex = 65;
+            this.Lblname.Text = "Silk City";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.ForeColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(63, 157);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(551, 4);
+            this.panel1.TabIndex = 64;
+            // 
+            // pbResturant
+            // 
+            this.pbResturant.Image = global::TripMaker.Properties.Resources.Train_Card;
+            this.pbResturant.Location = new System.Drawing.Point(7, 24);
+            this.pbResturant.Margin = new System.Windows.Forms.Padding(2);
+            this.pbResturant.Name = "pbResturant";
+            this.pbResturant.Size = new System.Drawing.Size(159, 116);
+            this.pbResturant.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResturant.TabIndex = 93;
+            this.pbResturant.TabStop = false;
+            // 
+            // TrainBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pbResturant);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aaa);
+            this.Controls.Add(this.aa);
+            this.Controls.Add(this.btnbook);
+            this.Controls.Add(this.lblprice);
+            this.Controls.Add(this.lblarvT);
+            this.Controls.Add(this.lblEnding);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblstart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbldepT);
+            this.Controls.Add(this.Lblname);
+            this.Controls.Add(this.panel1);
+            this.Name = "TrainBooking";
+            this.Size = new System.Drawing.Size(674, 172);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResturant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label aaa;
+        private System.Windows.Forms.Label aa;
+        private System.Windows.Forms.Button btnbook;
+        private System.Windows.Forms.Label lblprice;
+        private System.Windows.Forms.Label lblarvT;
+        private System.Windows.Forms.Label lblEnding;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblstart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbldepT;
+        private System.Windows.Forms.Label Lblname;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbResturant;
+    }
+}
